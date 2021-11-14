41f5a8382e965e0de1d93fd7c738ab8641cca708
PuneetSivananda <psivananda@chatsoft.comm>
2021-11-14 13:00:12 +0530

    feat: SVG element code

--- js/sight.js
+++ js/sight.js
@@ -1,8 +1,21 @@
 class SVGElement{
-    constructor(type){
+    constructor(type, attrs){
         this.type = type
         this.namespace = 'http://www.w3.org/2000/svg'
         this.node = document.createElementNS(this.namespace, this.type);
+       
         return this;
+    }
+    attr(attrs){
+        for (const [key, value] of  Object.entries(attrs)){
+            this.node.setAttributeNS(null, key, value)
+        }
+        return this
+    }
+
+    append(element){
+        const parent = typeof(element) === 'string' ? document.querySelector(element): element.node
+        parent.appendChild(this.node)
+        return this
     }
 }
